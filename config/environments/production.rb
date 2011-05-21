e49b89a00c660551941aa5fd82181cb30b5c45ec
Eric Soderberg <esoderi@gmail.com>
2011-05-21 07:00:37 -0700

    try sendmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Pbc3::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { :host => 'test.pbc.org' }
   
   config.time_zone = "Pacific Time (US & Canada)"
