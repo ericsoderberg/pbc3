4be3582ca8261ed05d933aef6e8ebe81dbd5d921
Eric Soderberg <esoderi@gmail.com>
2011-08-04 10:08:11 -0700

    allow youtube videos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110802090010) do
+ActiveRecord::Schema.define(:version => 20110804163635) do
 
   create_table "audios", :force => true do |t|
     t.string   "caption"
@@ -362,6 +362,7 @@ ActiveRecord::Schema.define(:version => 20110802090010) do
     t.integer  "page_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "youtube_id"
   end
 
 end
