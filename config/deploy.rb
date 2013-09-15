c3beb3b4c21e8ebdeb901a9e727ef00e3d73a356
Eric Soderberg <esoderi@gmail.com>
2013-09-14 17:29:46 -0700

    added solr to cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,11 +77,11 @@ after 'deploy:update_code', 'deploy:migrate'
 namespace :solr do
   desc "start solr"
   task :start, :roles => :app, :except => { :no_release => true } do 
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8080 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
   end
   desc "stop solr"
   task :stop, :roles => :app, :except => { :no_release => true } do 
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8080 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
   end
   desc "reindex the whole database"
   task :reindex, :roles => :app do
