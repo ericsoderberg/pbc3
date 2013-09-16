afad33c0f6da4c002c1cefefa819a246802216f5
Eric Soderberg <esoderi@gmail.com>
2013-09-16 16:58:59 -0700

    updated styling for better mobile support

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,37 +2,40 @@ class BooksController < ApplicationController
   
   def index
     @books = VerseParser::BIBLE_BOOKS.map{|b| b[0]}
-    @sections = []
+    @testaments = [{name: 'Old Testament', sections: []},
+      {name: 'New Testament', sections: []}]
+    testament = @testaments[0]
     section = nil
     @max_count = 0
     @books.each do |book|
       if 'Genesis' == book
         section = {:name => 'Pentatuch', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Joshua' == book
         section = {:name => 'History', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Job' == book
         section = {:name => 'Poetry', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Isaiah' == book
         section = {:name => 'Major Prophets', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Hosea' == book
         section = {:name => 'Minor Prophets', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Matthew' == book
+        testament = @testaments[1]
         section = {:name => 'Gospels', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Acts' == book
         section = {:name => 'Church History', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Romans' == book
         section = {:name => 'Epistles', :books => []}
-        @sections << section
+        testament[:sections] << section
       elsif 'Revelation' == book
         section = {:name => 'Prophecy', :books => []}
-        @sections << section
+        testament[:sections] << section
       end
       count = Message.count_for_book(book)
       section[:books] << {:book => book, :count => count}
