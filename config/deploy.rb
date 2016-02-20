ec256b3fbcd768cac5742dc1bf9e8bbfb5957354
Eric Soderberg <esoderi@gmail.com>
2016-02-20 07:20:33 -0800

    Simplified deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,16 @@
-# RVM bootstrap
-### switched to rbenv
-###$:.unshift(File.expand_path("~/.rvm/lib"))
-###require 'rvm/capistrano'
-###set :rvm_ruby_string, '1.9.2-p180'
-###set :rvm_type, :system
 
 # bundler bootstrap
 require 'bundler/capistrano'
 
 set :application, "pbc3.1"
 
-role :web, "www.pbc.org"                          # Your HTTP server, Apache/etc
-role :app, "www.pbc.org"                          # This may be the same as your `Web` server
-role :db,  "www.pbc.org", :primary => true # This is where Rails migrations will run
-#role :web, "test.pbc.org"                          # Your HTTP server, Apache/etc
-#role :app, "test.pbc.org"                          # This may be the same as your `Web` server
-#role :db,  "test.pbc.org", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+#!!!
+# role :web, "www.pbc.org"                          # Your HTTP server, Apache/etc
+# role :app, "www.pbc.org"                          # This may be the same as your `Web` server
+# role :db,  "www.pbc.org", :primary => true # This is where Rails migrations will run
+role :web, "test.pbc.org"                          # Your HTTP server, Apache/etc
+role :app, "test.pbc.org"                          # This may be the same as your `Web` server
+role :db,  "test.pbc.org", :primary => true # This is where Rails migrations will run
 
 #server
 default_run_options[:pty] = true
@@ -34,7 +28,7 @@ set :default_environment, {
 # repository
 set :scm, :git
 set :repository,  "git@github.com:ericsoderberg/pbc3.git"
-set :branch, "master"
+set :branch, "pbc4" #!!!
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
@@ -53,53 +47,6 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
-  task :setup_solr_data_dir do
-    run "mkdir -p #{shared_path}/solr/#{rails_env}"
-    run "mkdir -p #{shared_path}/solr/pids"
-    run "ln -nfs #{shared_path}/solr/#{rails_env} #{release_path}/solr/#{rails_env}"
-    run "ln -nfs #{shared_path}/solr/pids #{release_path}/solr/pids"
-  end
 end
-
-# http://www.hackido.com/2010/03/capistrano-sunspot-in-rails.html
-##task :before_update_code do
-##  stop solr:
-##  run "cd #{current_path} && rake sunspot:solr:stop RAILS_ENV=production"
-##end
 
 after 'deploy:update_code', 'deploy:migrate'
-
-#after "deploy:update_crontab", "deploy:solr:symlink"
-
-namespace :solr do
-  desc "start solr"
-  task :start, :roles => :app, :except => { :no_release => true } do 
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:start" #sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/#{rails_env}/data --pid-dir=#{shared_path}/pids/#{rails_env}"
-  end
-  desc "stop solr"
-  task :stop, :roles => :app, :except => { :no_release => true } do 
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:stop" #sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/#{rails_env}/data --pid-dir=#{shared_path}/pids/#{rails_env}"
-  end
-  desc "reindex the whole database"
-  task :reindex, :roles => :app do
-    stop
-    run "rm -rf #{shared_path}/solr/production/data"
-    start
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex"
-  end
-end
-
-after 'deploy:update_code', 'deploy:setup_solr_data_dir'
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
